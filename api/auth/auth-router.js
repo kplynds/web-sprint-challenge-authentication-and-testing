c4ae06160a07c98c3cad9a14c9d6d9c753c8a103
Kyle Lynds <kylelynds@Kyles-MBP.lan>
2020-12-17 22:50:23 -0800

    register endpoint working ex for username taken

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,8 +1,67 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const db = require("../../data/dbConfig");
+const jwt = require("jsonwebtoken");
+const jwtSecret = require("../jwtSecret");
 
-router.post('/register', (req, res) => {
-  res.end('implement register, please!');
-  /*
+const checkIfPayloadExists = (req, res, next) => {
+  if (!req.body.username || !req.body.password) {
+    res.status(401).json("username and password required");
+  } else {
+    next();
+  }
+};
+
+const checkIfUsernameIsUnique = async (req, res, next) => {
+  try {
+    const rows = db("users")
+      .where({ username: req.body.username })
+      .orderBy("id")
+    if (!rows.length) {
+      next();
+    } else {
+      res.status(401).json("username taken");
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const makeToken = (user) => {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+  const options = {
+    expiresIn: "900s",
+  };
+  return jwt.sign(payload, jwtSecret, options);
+};
+
+router.post(
+  "/register",
+  checkIfPayloadExists,
+  checkIfUsernameIsUnique,
+  (req, res) => {
+    console.log("registering");
+    const hash = bcrypt.hashSync(req.body.password, 10);
+
+    const newUser = {
+      username: req.body.username,
+      password: hash,
+    };
+
+    db("users")
+      .insert(newUser)
+      .then(([data]) => {
+        res
+          .status(201)
+          .json({ username: newUser.username, password: newUser.password, id: data });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.code });
+      });
+    /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
 
@@ -26,10 +85,11 @@ router.post('/register', (req, res) => {
     4- On FAILED registration due to the `username` being taken,
       the response body should include a string exactly as follows: "username taken".
   */
-});
+  }
+);
 
-router.post('/login', (req, res) => {
-  res.end('implement login, please!');
+router.post("/login", (req, res) => {
+  res.end("implement login, please!");
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
